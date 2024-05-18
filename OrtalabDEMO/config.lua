1fb755fa5e0adc17e116d7d56479ebd4c307af5a
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-05-18 08:03:33 -0800

    Add files via upload

---
+++ OrtalabDEMO/config.lua
@@ -0,0 +1,28 @@
+-- Config file for Ortalab
+local decks_enabled = true --Enabled/Disable Decks
+local jokers_enabled = true --Enabled/Disable Jokers
+local loterias_enabled = true --Enabled/Disable Loteria
+local mythos_enabled = true --Enabled/Disable Mythos
+local zodiacs_enabled = true --Enabled/Disable Zodiac
+local coupons_enabled = true --Enabled/Disable Coupons
+local tags_enabled = true --Enabled/Disable Tags
+local editions_enabled = true --Enabled/Disable Editions
+local curses_enabled = true --Enabled/Disable Curses + Objects relating to curses
+local enhancements_enabled = true --Enabled/Disable Enhancements + Objects relating to enhancements
+local blinds_enabled = true --Enabled/Disable Blinds
+local challenges_enabled = true --Enabled/Disable Challenges
+
+return {
+	decks_enabled = decks_enabled,
+	jokers_enabled = jokers_enabled,
+	loterias_enabled = loterias_enabled,
+	mythos_enabled = mythos_enabled,
+	zodiacs_enabled = zodiacs_enabled,
+	coupons_enabled = coupons_enabled,
+	tags_enabled = tags_enabled,
+	editions_enabled = editions_enabled,
+	curses_enabled = curses_enabled,
+	enhancements_enabled = enhancements_enabled,
+	blinds_enabled = blinds_enabled,
+	challenges_enabled = challenges_enabled,
+}
