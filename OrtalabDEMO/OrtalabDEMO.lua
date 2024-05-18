f1e7615c77bfea33f95f24495abd88fe1aeffb5a
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2024-05-18 08:03:21 -0800

    Update OrtalabDEMO.lua

--- OrtalabDEMO/OrtalabDEMO.lua
+++ OrtalabDEMO/OrtalabDEMO.lua
@@ -2,7 +2,7 @@
 --- MOD_NAME: Ortalab DEMO (0.8.0)
 --- MOD_ID: OrtalabDEMO
 --- MOD_AUTHOR: [Crimson Heart, Balatro Discord]
---- MOD_DESCRIPTION: Every action has an opposite reaction. In another world, in the nation of "Virtue", a simple indie developer created Ortalab, which so happened to be the opposite of our world's Balatro. This mod is intended to port everything from that parallel world to Balatro. Within this demo, it includes 40 Jokers and 3 decks for you to check out.
+--- MOD_DESCRIPTION: Every action has an opposite reaction. In another world, in the nation of "Virtue", a simple indie developer created Ortalab, which so happened to be the opposite of our world's Balatro. This mod is intended to port everything from that parallel world to Balatro. Within this demo, it includes 45 Jokers, 4 Decks, 2 sets of Coupons, and 2 Challenges for you to check out.
 --- DISPLAY_NAME: Ortalab DEMO
 --- BADGE_COLOUR: 990000
 --- PREFIX: olab
@@ -15,15 +15,19 @@ function loc_colour(_c, _default)
     loc_colour_ref(_c, _default) -- toss the return value, we don't need it
     G.ARGS.LOC_COLOURS['zodiac'] = HEX('9D3B35')
     G.ARGS.LOC_COLOURS['loteria'] = HEX('B77A91')
-    G.ARGS.LOC_COLOURS['cryptid'] = HEX('704F72')
+    G.ARGS.LOC_COLOURS['mythos'] = HEX('704F72')
     return G.ARGS.LOC_COLOURS[_c] or _default or G.C.UI.TEXT_DARK -- recalculate result
 end
 
-SMODS.Sprite{key = "Ortalab_Vouchers", path = "Ortalab_Vouchers.png", px = 71, py = 95, atlas = "ASSET_ATLAS"}:register()
-SMODS.Sprite{key = "Ortalab_Jokers", path = "Ortalab_Jokers.png", px = 71, py = 95, atlas = "ASSET_ATLAS"}:register()
-SMODS.Sprite{key = "Ortalab_Enhancers", path = "Ortalab-Enhancers.png", px = 71, py = 95, atlas = "ASSET_ATLAS"}:register()
+SMODS.Atlas{key = "Ortalab_Vouchers", path = "Ortalab_Vouchers.png", px = 71, py = 95}
+SMODS.Atlas{key = "Ortalab_Jokers", path = "Ortalab_Jokers.png", px = 71, py = 95}
+SMODS.Atlas{key = "Ortalab_Enhancers", path = "Ortalab-Enhancers.png", px = 71, py = 95}
 
 local mod_path = SMODS.current_mod.path
+
+local config = NFS.load(mod_path.."config.lua")()
+SMODS.Mods.OrtalabDEMO["config"] = config
+
 local file_groups = NFS.getDirectoryItems(mod_path.."Objects")
 local function init_file_groups()
 	for k, file_group in pairs(file_groups) do
@@ -32,7 +36,7 @@ local function init_file_groups()
 			if string.find(v, ".lua") then
 				local init_obj_filepath = tostring(mod_path.."Objects/"..file_group)
 				local init_obj_file = NFS.load(mod_path.."Objects/"..file_group.."/"..v)()
-				init_obj_file(init_obj_filepath)
+				if SMODS.Mods.OrtalabDEMO.config[init_obj_file.group_config_enable] == true then init_obj_file.init_func(init_obj_filepath) end
 			end
 		end
 	end
